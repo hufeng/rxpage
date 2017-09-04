65a6e416c56b00b6e52d2f144e6bf84c70c63b30
hufeng <hufeng@qianmi.com>
2017-09-04 16:22:30 +0800

    auto merge lifecycle method

--- __tests__/index-test.ts
+++ __tests__/index-test.ts
@@ -1,9 +1,9 @@
-import { PageFactory } from '../src/index';
+import { PageParser } from '../src/index';
 
 test('basic', () => {
   const hello = () => {};
 
-  const page = new PageFactory({
+  const page = new PageParser({
     data: {
       id: 1
     },
@@ -29,7 +29,7 @@ test('mixins', () => {
     sayWorld() {}
   };
 
-  const page = new PageFactory({
+  const page = new PageParser({
     data: { id: 1 },
     mixins: [HelloPage, WorldPage]
   });
@@ -49,7 +49,7 @@ test('mixins', () => {
     mixins: [Nest]
   };
 
-  const page1 = new PageFactory({
+  const page1 = new PageParser({
     data: {
       id: 2,
       name: 'rxpage'
@@ -58,4 +58,81 @@ test('mixins', () => {
   });
 
   expect(page1).toMatchSnapshot();
+});
+
+test('lifecycle method', () => {
+  const Btn = {
+    data: {
+      btn: 'hello btn'
+    },
+    mixins: [
+      {
+        onLoad() {
+          console.log('btn nested onload');
+        },
+        onUnload() {
+          console.log('btn nested unload');
+        }
+      }
+    ],
+
+    onLoad() {
+      console.log('btn onload');
+    },
+    onShow() {
+      console.log('btn show');
+    },
+    onUnload() {
+      console.log('btn unload');
+    }
+  };
+
+  const LoadingMore = {
+    data: {
+      text: 'loading...'
+    },
+    onLoad() {
+      console.log('loading more onload');
+    },
+    onShow() {
+      console.log('loading more show');
+    },
+    onUnload() {
+      console.log('loading more unload');
+    }
+  };
+
+  const page = new PageParser({
+    data: {
+      hello: 'hello'
+    },
+    mixins: [Btn, LoadingMore],
+    hello() {
+      console.log('hello');
+    },
+    onLoad() {
+      console.log('page parse onload');
+    },
+    onUnload() {
+      console.log('page parse unload');
+    },
+    onShow() {
+      console.log('page parse show');
+    }
+  });
+
+  expect(page).toMatchSnapshot();
+  expect(page.getPage()).toMatchSnapshot();
+
+  const pageObj = page.getPage() as {
+    hello: Function;
+    onLoad: Function;
+    onShow: Function;
+    onUnload: Function;
+  };
+
+  pageObj.hello();
+  pageObj.onLoad();
+  pageObj.onUnload();
+  pageObj.onShow();
 });
--- src/index.ts
+++ src/index.ts
@@ -10,35 +10,66 @@ export interface IProps {
   [name: string]: any;
 }
 
+//小程序page生命周期方法
+const lifecycleMethodName = [
+  'onLoad',
+  'onReady',
+  'onShow',
+  'onHide',
+  'onUnload',
+  'onPullDownRefresh',
+  'onReachBottom',
+  'onPageScroll'
+];
+
 /**
  * RxPage
  * 1. combine datasource
  * 2. combine component
+ * 3. auto merge lifecycle method
  */
-export class PageFactory {
-  private _page: Object;
+export class PageParser {
+  //转换page的属性对象
+  private page: Object;
+  //生命周期方法的栈
+  private lifecycleMethodStack: {
+    [name: string]: Array<Function>;
+  };
 
   constructor(props) {
-    this._page = {
+    this.lifecycleMethodStack = {
+      onLoad: [],
+      onReady: [],
+      onShow: [],
+      onHide: [],
+      onUnload: [],
+      onPullDownRefresh: [],
+      onReachBottom: [],
+      onShareAppMessage: [],
+      onPageScroll: []
+    };
+
+    this.page = {
       data: {}
     };
+
     this.parseProps(props);
   }
 
   private parseProps(props: IProps) {
-    //先merge mixins, 因为data优先级最高，data可以覆盖mixins的值
-    this.parseMixin(props);
+    //拆分data，mixins和其他的属性
+    let { data = {}, mixins = [], ...other } = props;
 
-    const { data = {}, mixins, ...other } = props;
+    //对其他的属性分离生命周期方法
+    this.parseLifeCycleMethod(other);
+
+    //先merge mixins, 因为data优先级最高，data可以覆盖mixins的值
+    mixins.forEach(mixin => this.parseMixin(mixin));
+
     //data
-    this._page['data'] = {
-      ...this._page['data'],
+    this.page['data'] = {
+      ...this.page['data'],
       ...data
-    };
-    //others
-    this._page = {
-      ...this._page,
-      ...other
     };
   }
 
@@ -46,26 +77,78 @@ export class PageFactory {
     const { data = {}, mixins = [], ...other } = props;
 
     //merge data
-    this._page['data'] = {
-      ...this._page['data'],
+    this.page['data'] = {
+      ...this.page['data'],
       ...data
     };
+
+    this.parseLifeCycleMethod(other);
 
     //如果存在mixin, 递归mixin
     if (mixins.length > 0) {
       mixins.forEach((page: IProps) => this.parseMixin(page));
     }
+  }
 
-    //merge others
-    this._page = { ...this._page, ...other };
+  /**
+   * 分离生命周期方法和普通的方法
+   * 生命周期方法，合并处理
+   * @param obj 
+   */
+  parseLifeCycleMethod(obj: Object) {
+    if (!obj) {
+      return;
+    }
+
+    const others = {};
+    Object.keys(obj).forEach(key => {
+      const value = obj[key];
+      const isLifeCycleMethod = lifecycleMethodName.indexOf(key) != -1;
+
+      if (isLifeCycleMethod) {
+        this.lifecycleMethodStack[key].push(value);
+      } else {
+        others[key] = value;
+      }
+    });
+
+    this.page = {
+      ...this.page,
+      ...others
+    };
+  }
+
+  mixinLifeCycle() {
+    const lifecycleMethodStack = this.lifecycleMethodStack;
+    Object.keys(lifecycleMethodStack).forEach(name => {
+      const stack = lifecycleMethodStack[name];
+      if (stack.length > 0) {
+        //如果是销毁方法，按栈的顺序
+        //如果是普通的生命周期方法，按照栈的反序
+        if (name === 'onUnload') {
+          this.page['onUnload'] = function() {
+            stack.forEach(method => method());
+          };
+        } else if (name === 'onLoad') {
+          this.page['onLoad'] = function(opts) {
+            stack.reverse().forEach(method => method(opts));
+          };
+        } else {
+          this.page[name] = function() {
+            stack.reverse().forEach(method => method());
+          };
+        }
+      }
+    });
   }
 
   getPage() {
-    return this._page;
+    this.mixinLifeCycle();
+    return this.page;
   }
 }
 
 export default function RxPage(opts) {
-  const factory = new PageFactory(opts);
+  const factory = new PageParser(opts);
   return Page(factory.getPage());
 }
